9f7df856628ae06b6a93b8a01d036967e1d8348e
skorab <skorab@gmx.de>
2016-06-17 00:56:52 +0200

    Update pegida.py

--- pegida.py
+++ pegida.py
@@ -1,8 +1,8 @@
-background_image_filename = "Dresden_Map.svg"
-lachmann_image_filename = "lachmann.svg"
-oertel_image_filename = "oertel.svg"
-festerling_image_filename = "festerling.svb"
-tonnenanwalt_image_filename = "tonnenanwalt.svg"
+background_image_filename = "Dresden_Map.png"
+lachmann_image_filename = "lachmann.png"
+oertel_image_filename = "oertel.png"
+festerling_image_filename = "festerling.png"
+tonnenanwalt_image_filename = "tonnenanwalt.png"
 
 
 import pygame
