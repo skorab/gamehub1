f4d09c024e9e26d83f93357cf22bf3fb4034c57f
skorab <skorab@gmx.de>
2016-06-17 16:35:21 +0200

    screen_size

--- pegida.py
+++ pegida.py
@@ -4,15 +4,19 @@ oertel_image_filename = "oertel.png"
 festerling_image_filename = "festerling.png"
 tonnenanwalt_image_filename = "tonnenanwalt.png"
 
+
 import pygame
 from math import *
 from pygame.locals import *
 from sys import exit
+pygame.init()
 import numpy
 
-pygame.init()
-screen = pygame.display.set_mode((1920, 1080), 0, 32)
+screen_size = (980, 735)
+
+screen = pygame.display.set_mode(screen_size, RESIZABLE, 32)
 pygame.display.set_caption("Scheiss Pegida")
+
 
 background = pygame.image.load(background_image_filename).convert()
 lachmann = pygame.image.load(lachmann_image_filename).convert_alpha()
@@ -21,27 +25,28 @@ festerling = pygame.image.load(festerling_image_filename).convert_alpha()
 tonnenanwalt = pygame.image.load(tonnenanwalt_image_filename).convert_alpha()
 
 def get_heading(nextpoint, position_):
-
-#get_heading berechnet den Richtungsvektor und den Normierten-Vektor 
-#zwischen der aktuellen Position (position_) und dem naechsten Wegpunkt (nextpoint).
-#Get heading muss nur ein einziges mal pro Wegpunkt ausgefuehrt werden.
     
+    #print(nextpoint[0])
+    #print(position_[0])
     richtungsvektor = [0,0]
     normalvektor = [0,0]
+    
     richtungsvektor = [nextpoint[0] - position_[0],
         nextpoint[1] - position_[1]]
     
     betrag = sqrt( richtungsvektor[0]**2 + richtungsvektor[1]**2 )
+
     normalvektor = [(richtungsvektor[0] / betrag ),(richtungsvektor[1] / betrag)]
 
+    print("Richtungsvektor:")
+    print(richtungsvektor)
+    
+    print("NormalVektor:")
+    print(normalvektor)
     return richtungsvektor 
 
 
 def step(state, speed):
-    """step() fuehrt einen einzelnen schritt in richtung des naechsten wegpunktes aus. 
-       Die Fkt. addiert das Heading * Geschwindigkeit auf die Aktuelle position. 
-       Folglich verschiebt sich die Position einen Schritt in richtung des naechsten Wegpunktes"""
-
     old = start
 
     start[0] += (heading[0] * speed)
@@ -50,18 +55,14 @@ def step(state, speed):
 
 
 def check_waypoint(counter_):
-    """check_waypoint prueft ob der Wegpunkt erreicht wurde. 
-    Er vergleicht die x und y koordinaten von position und wegpunkt, sind sie identisch wurde der Wegpunkt
-    erreicht. Aufgrund von Rundungsfehlern werden die Koordinaten im vorraus auf 2 stellen nach dem Komma 
-    gerundet."""
-
     global counter, heading
-    x1 = round(waypoints[counter_][0], 2)
-    y1 = round(waypoints[counter_][1], 2)
-    x2 = round(start[0], 2)
-    y2 = round(start[1], 2)
-    
-    if x1  == x2 and y1 == y2:
+    a1 = round(waypoints[counter_][0], 2)
+    a2 = round(waypoints[counter_][1], 2)
+
+    b1 = round(start[0], 2)
+    b2 = round(start[1], 2)
+    if a1  == b1 and a2 == b2:
+        print("hi")
         counter += 1
         heading = get_heading(waypoints[counter], start)
 
@@ -72,12 +73,21 @@ old = []
 heading = get_heading(waypoints[0],start)
 counter = 0
 
+
 while True:
     time += 0.01
     for event in pygame.event.get():
 	    
         if event.type == QUIT:
             exit()
+        if event.type == VIDEORESIZE:
+        	screen_size = event.size
+        	screen = pygame.display.set_mode(screen_size, RESIZABLE, 32)
+        	
+        screen_width, screen_height = screen_size
+        for y in range(0, screen_height, background.get_height()):
+        	for x in (0, screen_width, background.get_width()):
+        		screen.blit(background, (x,y))
 			
     screen.blit(background, (0.0,0.0))
     screen.blit(lachmann, step(1,0.0005))
